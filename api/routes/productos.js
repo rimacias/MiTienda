31c70ee9dd1371c4a5f3a485cd32441794cd84c7
Sergio Basurto <sbasurto@espol.edu.ec>
2023-08-26 18:45:39 -0500

    Agregación de funcionalidades en API

--- api/routes/productos.js
+++ api/routes/productos.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const Producto = require('../models/producto');
+const db = require('../models');
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -8,12 +8,90 @@ router.get('/', function(req, res, next) {
 
 router.get('/all', async (req, res) => {
     try {
-      const productos = await Producto.findAll(); // Consulta todos los productos
+      const productos = await db.Producto.findAll();
       res.json(productos);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Error al consultar productos.' });
     }
 });
+
+router.get('/:id', async (req, res) => {
+  const productId = req.params.id;
+
+  try {
+    const producto = await db.Producto.findByPk(productId);
+
+    if (!producto) {
+      return res.status(404).json({ error: 'Producto no encontrado.' });
+    }
+
+    res.json(producto);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al consultar el producto.' });
+  }
+});
+
+router.post('/new', async (req, res) => {
+  const { nombre, precio, descripcion } = req.body;
+
+  try {
+    const nuevoProducto = await db.Producto.create({
+      nombre,
+      precio,
+      descripcion,
+    });
+
+    res.status(201).json(nuevoProducto);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al crear el producto.' });
+  }
+});
+
+router.put('/update/:id', async (req, res) => {
+  const productId = req.params.id;
+  const { nombre, precio, descripcion } = req.body;
+
+  try {
+    const producto = await db.Producto.findByPk(productId);
+
+    if (!producto) {
+      return res.status(404).json({ error: 'Producto no encontrado.' });
+    }
+
+    await producto.update({
+      nombre,
+      precio,
+      descripcion,
+    });
+
+    res.json(producto);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al actualizar el producto.' });
+  }
+});
+
+router.delete('/delete/:id', async (req, res) => {
+  const productId = req.params.id;
+
+  try {
+    const producto = await db.Producto.findByPk(productId);
+
+    if (!producto) {
+      return res.status(404).json({ error: 'Producto no encontrado.' });
+    }
+
+    await producto.destroy();
+
+    res.json({ message: 'Producto eliminado exitosamente.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al eliminar el producto.' });
+  }
+});
+
 
 module.exports = router;
